49040c23ac5785706e9e0ae29f859b59324b9e01
Heinen,S.A. (Steven) <s.a.heinen@students.uu.nl>
2020-03-30 21:14:08 +0200

    change program to test io

--- AN-Project/AN-Project/Program.cs
+++ AN-Project/AN-Project/Program.cs
@@ -6,9 +6,18 @@ namespace AN_Project
 {
     class Program
     {
+        public static UndirectedGraph inputGraph;
+
         static void Main(string[] args)
         {
             Console.WriteLine("A&N project");
+
+            string filepath = "..\\..\\..\\..\\..\\Testcases\\heur_001.gr";
+            inputGraph = IO.ReadInput(filepath);
+
+            State initialState = BaseSolutionGenerator.Empty();
+
+            string output = IO.WriteOutput(initialState);
         }
     }
 }
